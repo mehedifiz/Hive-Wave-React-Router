43806364dcbf8a3b26db6d7c553d0409a5d8c523
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-05-09 12:11:25 +0600

    nav seted

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
 import './App.css'
+import Home from './Pages/Home';
 
 function App() {
 
   return (
     <>
       
+        
+      <Home></Home>
 
 
 
 
-      
-    </>
+      </>
   );
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import Nav from "../Comp/Nav";
+
+const Home = () => {
+    return (
+        <div>
+
+<Nav></Nav>
+
+            <h1>home</h1>
+
+
+        </div>
+    );
+};
+
+export default Home;
